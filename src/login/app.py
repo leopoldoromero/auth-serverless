c6a49ededeac01019f77e3a9e37ab42e595cc95e
Leopoldo <leopoldo.romero@powen.es>
2024-07-15 00:20:41 +0200

    fixed error in login handler when validating passwords

--- src/login/app.py
+++ src/login/app.py
@@ -9,6 +9,8 @@ dynamodb = boto3.resource('dynamodb')
 user_table = dynamodb.Table(os.environ['DYNAMODB_TABLE_NAME'])
 
 def check_password(stored_password, provided_password):
+    if isinstance(stored_password, str):
+      stored_password = bytes.fromhex(stored_password)
     salt = stored_password[:16]
     stored_password = stored_password[16:]
     hashed_password = hashlib.pbkdf2_hmac('sha256', provided_password.encode('utf-8'), salt, 100000)
@@ -21,11 +23,13 @@ def login_handler(event, context):
     password = body['password']
 
     try:
+        print(f"Querying for email: {email}")
         response = user_table.query(
             IndexName='EmailIndex',
             KeyConditionExpression='email = :email',
             ExpressionAttributeValues={':email': email}
         )
+        print("DynamoDB response: ", response)
         
         if 'Items' not in response or len(response['Items']) == 0:
             return {
@@ -38,13 +42,13 @@ def login_handler(event, context):
         user_id = user['id']
         user_name = user['userName']
         
-        if check_password(stored_password, password):
+        if not check_password(stored_password, password):
              return {
                 'statusCode': 400,
                 'body': json.dumps({'message': 'Login failed', 'error': 'Incorrect email or password'})
             }
 
-
+        print(f"Initiating auth for user: {user_name}")
         cognito_response = cognito_client.initiate_auth(
             AuthFlow='USER_PASSWORD_AUTH',
             AuthParameters={
@@ -53,6 +57,9 @@ def login_handler(event, context):
             },
             ClientId=os.environ['USER_POOL_CLIENT_ID']
         )
+        print("Cognito response: ", cognito_response)
+
+        print(f"Updating login status for user ID: {user_id}")
         user_table.update_item(
             Key={'id': user_id},
             UpdateExpression='SET #status = :val1',
@@ -70,4 +77,11 @@ def login_handler(event, context):
         return {
             'statusCode': 400,
             'body': json.dumps({'message': 'Login failed', 'error': str(e)})
+        }
+        
+    except Exception as e:
+        print("Exception: ", str(e))
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Internal server error', 'error': str(e)})
         }
